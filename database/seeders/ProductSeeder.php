f6c426775e345c81cac11bc48190f0dd56ab1169
Daniel Neto <daniel@danielneto.dev.br>
2022-10-01 18:00:21 -0300

    implements seeder of fake data

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -2,17 +2,15 @@
 
 namespace Database\Seeders;
 
+use App\Models\Product;
 use Illuminate\Database\Seeder;
 
 class ProductSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
+    public function run(): void
     {
-        //
+        Product::factory()
+            ->count(100)
+            ->create();
     }
 }
--- database/seeders/SaleSeeder.php
+++ database/seeders/SaleSeeder.php
@@ -2,17 +2,34 @@
 
 namespace Database\Seeders;
 
+use App\Actions\Sales\CreateSaleAction;
+use App\Models\{Customer, Product};
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Collection;
 
 class SaleSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
+    public function run(): void
     {
-        //
+        Collection::times(50, static function () {
+            /** @var Customer $customer */
+            $customer = Customer::query()
+                ->inRandomOrder()
+                ->first();
+
+            $products = Product::query()
+                ->inRandomOrder()
+                ->take(4)
+                ->get()
+                ->map(fn (Product $product) => [
+                    'id' => $product->id,
+                    'quantity' => random_int(1, 10),
+                ]);
+
+            (new CreateSaleAction())->execute([
+                'customer_id' => $customer->id,
+                'products' => $products->toArray(),
+            ]);
+        });
     }
 }
